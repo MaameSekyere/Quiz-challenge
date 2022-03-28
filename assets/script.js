a8c5fde4bf7cea387b0dcf437ccdac687d9dadd7
MaameSekyere <maameant@gmail.com>
2022-03-27 22:45:12 -0400

    added questions/answers to code quiz

--- assets/script.js
+++ assets/script.js
@@ -42,19 +42,12 @@ let myQuestions = [
 console.log(myQuestions);
 
 //Selecting all elements
-var startButtonEl = document.getElementById("start-button");
-var exitButtonEl = document.getElementById("#exit-button");
-var restartButtonEl = document.querySelector("#restart-button");
-var quitButtonEl = document.querySelector("#quit-button");
-var quizBoxEl = document.querySelector("#quiz-box");
-var optionListEl = document.querySelector("#option-list");
-//var options = document.createElement("p");
+var quizContainer = document.getElementById("quiz");
+var resultsContainer = document.getElementById("results");
+var submitButton = document.getElementById("submit");
 var informationBox = document.querySelector(".Information-Box");
-var resultBox = document.querySelector(".result-box");
-var highscoreEl = document.querySelector(".setHighscore");
-var mainQuestion = document.querySelector("#main-question");
-var nextButton = document.querySelector("#next-button");
-mainQuestion.innerHTML = "";
+var startButtonEl = document.querySelector("#start-button");
+var quizBox = document.querySelector("#quiz-box");
 
 var timer;
 var time = 60;
@@ -62,11 +55,107 @@ var setTime = document.querySelector("#timer");
 
 setTime.textContent = "Time Left: " + time;
 
+generateQuiz(myQuestions, quizContainer, resultsContainer, submitButton);
+
+function generateQuiz(
+  questions,
+  quizContainer,
+  resultsContainer,
+  submitButton
+) {
+  function showQuestions(questions, quizContainer) {
+    // we'll need a place to store the output and the answer choices
+    var output = [];
+    var answers;
+
+    // for each question...
+    for (var i = 0; i < questions.length; i++) {
+      // first reset the list of answers
+      answers = [];
+
+      // for each available answer...
+      for (letter in questions[i].answers) {
+        // ...add an html radio button
+        answers.push(
+          "<label>" +
+            '<input type="radio" name="question' +
+            i +
+            '" value="' +
+            letter +
+            '">' +
+            letter +
+            ": " +
+            questions[i].answers[letter] +
+            "</label>"
+        );
+      }
+
+      // add this question and its answers to the output
+      output.push(
+        '<div class="question">' +
+          questions[i].question +
+          "</div>" +
+          '<div class="answers">' +
+          answers.join("") +
+          "</div>"
+      );
+    }
+
+    // finally combine our output list into one string of html and put it on the page
+    quizContainer.innerHTML = output.join("");
+  }
+
+  function showResults(questions, quizContainer, resultsContainer) {
+    // gather answer containers from our quiz
+    var answerContainers = quizContainer.querySelectorAll(".answers");
+
+    // keep track of user's answers
+    var userAnswer = "";
+    var numCorrect = 0;
+
+    // for each question...
+    for (var i = 0; i < questions.length; i++) {
+      // find selected answer
+      userAnswer = (
+        answerContainers[i].querySelector(
+          "input[name=question" + i + "]:checked"
+        ) || {}
+      ).value;
+
+      // if answer is correct
+      if (userAnswer === questions[i].correctAnswer) {
+        // add to the number of correct answers
+        numCorrect++;
+
+        // color the answers green
+        answerContainers[i].style.color = "lightgreen";
+      }
+      // if answer is wrong or blank
+      else {
+        // color the answers red
+        answerContainers[i].style.color = "black";
+      }
+    }
+
+    // show number of correct answers out of total
+    resultsContainer.innerHTML = numCorrect + " out of " + questions.length;
+  }
+
+  // show questions right away
+  showQuestions(questions, quizContainer);
+
+  // on submit, show results
+  submitButton.onclick = function () {
+    showResults(questions, quizContainer, resultsContainer);
+  };
+}
+
 function startQuiz() {
   // Loop through questions
   informationBox.style.display = "none";
-  resultBox.style.display = "none";
-  highscoreEl.style.display = "none";
+
+  //resultBox.style.display = "none";
+  //highscoreEl.style.display = "none";
 
   timer = setInterval(tickDown, 1000);
 }
@@ -76,37 +165,6 @@ function tickDown() {
   setTime.textContent = "Time Left: " + time;
   if (time <= 0) {
     endQuiz();
-  }
-}
-function getQuestions() {
-  // for loop to loop through each questions
-
-  for (let i = 0; i < questions.length; i++) {
-    // display questions and options onto page by targeting the html elements in HTML  (main-question, option-list)
-    //make the options clickable so we can store the data
-    var a1 = (options.textContent = questions[i].options[0]);
-    var a2 = (options.textContent = questions[i].options[1]);
-    var a3 = (options.textContent = questions[i].options[2]);
-    var a4 = (options.textContent = questions[i].options[3]);
-    a1.setAttribute("value", questions[i].options[0]);
-    a2.setAttribute("value", questions[i].options[1]);
-    a3.setAttribute("value", questions[i].options[2]);
-    a4.setAttribute("value", questions[i].options[3]);
-    a1.append(optionListEl);
-    a2.append(optionListEl);
-    a3.append(optionListEl);
-    a4.append(optionListEl);
-
-    //if this.value is not1 the same as questions[i].answer
-    // Penalized the time
-    // when the loop ends or when the time runs out endQuiz
-    console.log(questions[i].answer);
-
-    //   if (this.value !== questions[i].answer) {
-    // time -=10
-    if (this.value !== questions[0].answer) {
-      time -= 10;
-    } else getQuestions();
   }
 }
 
@@ -119,5 +177,3 @@ function endQuiz() {
   // Once they click submit, it will send them to the highscore page
 }
 startButtonEl.onclick = startQuiz;
-
-//when the submit button for the highscore is clicked, take the user to the highscore html page
